65aad4c5762a7ae30d0b3fe1878d4e2f7307ae1e
milky <milky@s4>
2021-12-06 08:55:10 +0100

    day 6 part 2

--- p6.rs
+++ p6.rs
@@ -7,7 +7,7 @@ use super::aocutil;
 pub fn p6() -> String {
     let lanternfishs = aocutil::read_file_line_to_u8_list("p6_1".to_string());
     // let lanternfishs : Vec<u8> = vec![3,4,3,1,2];
-    let mut counters : [u32;9] = [0;9];
+    let mut counters : [u128;9] = [0;9];
     for fish in lanternfishs {
         counters[fish as usize] += 1;
     }
@@ -21,11 +21,24 @@ pub fn p6() -> String {
         counters[8] = gen;
         // println!("day {} {:?}", day, counters);
     }
-    let mut sum : u32 = 0;
+    let mut sum : u128 = 0;
     for c in counters {
-        sum += (c as u32);
+        sum += c;
     }
-
-    format!("{:?}-{:?}", sum, 0).to_string()
+    for day in 81..257 {
+        let gen = counters[0];
+        for n in 0..7 {
+            counters[n] = counters[n+1];
+        }
+        counters[6] = gen + counters[7];
+        counters[7] = counters[8];
+        counters[8] = gen;
+        // println!("day {} {:?}", day, counters);
+    }
+    let mut sum2 : u128 = 0;
+    for c in counters {
+        sum2 += c;
+    }
+    format!("{:?}-{:?}", sum, sum2).to_string()
 }
 
